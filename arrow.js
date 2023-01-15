ce60a8616c96894eaadde1602ae638d1d09a948a
Panicshoe <86228058+Panicshoe@users.noreply.github.com>
2023-01-15 14:27:14 -0500

    initial commit

---
+++ arrow.js
@@ -0,0 +1,18 @@
+/*function sum1(a,b)
+{
+    return a + b;
+ }
+ let result = sum1(5,3);
+ console.log(result);*/
+
+ /*const sum1 = (a,b) =>
+ {
+    return a + b;
+ }
+ let result = sum1(5,4);
+ console.log(result);*/
+
+ const sum1 = (a,b) => a +b;
+ 
+ let result = sum1(9,4);
+ console.log(result);
